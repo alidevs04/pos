a0ad9a5149efa328bc5e52ae14a26326225ca722
Ali <aliaisrar7@gmail.com>
2025-04-13 15:40:21 +0300

    Update ALLOWED_HOSTS to include Render domain

--- pointofsales/settings.py
+++ pointofsales/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,17 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_&mo7hk1&(g15876vtgisx&ixk^x!*rsdh52liim$$ifv3p49k'
+SECRET_KEY = SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'pointofsales',
     'django.contrib.admin',
@@ -42,6 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -76,12 +80,12 @@ WSGI_APPLICATION = 'pointofsales.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.trqmfwubklphevypsmpd',
-        'PASSWORD': 'Seasaw22',
-        'HOST': 'aws-0-eu-central-1.pooler.supabase.com',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),  # The database name
+        'USER': os.getenv('DB_USER'),  # The database username
+        'PASSWORD': os.getenv('DB_PASSWORD'),  # The database password
+        'HOST': os.getenv('DB_HOST'),  # The database host URL
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
@@ -126,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
