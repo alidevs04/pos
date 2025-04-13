5da057f67698f75565c62d9e2ae50b9f2fc28d1e
Ali <aliaisrar7@gmail.com>
2025-04-13 21:07:39 +0300

    fix 8.0

--- pointofsales/settings.py
+++ pointofsales/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'pointofsales.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DB_NAME',
-        'USER': 'DB_USER',
-        'PASSWORD': 'DB_PASSWORD',
-        'HOST': 'DB_HOST',
-        'PORT': 'DB_PORT',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),  # ← this is the problem
     }
 }
 
