172d23824ecbc736bc690ce7661439a1036717a6
Ali <aliaisrar7@gmail.com>
2025-04-15 17:35:21 +0300

    get orders

--- orderdetails/views.py
+++ orderdetails/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import OrderSerializer
+from .models import Order
 # Create your views here.
 
 @api_view(['POST', 'GET'])
@@ -12,3 +13,8 @@ def reg_order(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+    if request.method == 'GET':
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
