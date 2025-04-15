4c854d0d5f9de767c0f94d8e1505b67d680702b7
Ali <aliaisrar7@gmail.com>
2025-04-15 18:25:33 +0300

    fixed get sale this month

--- orderdetails/urls.py
+++ orderdetails/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('orders', views.reg_order),
-    path('orders/saletoday', views.total_sales_today)
+    path('orders/saletoday', views.total_sales_today),
+    path('orders/salethismonth', views.sales_this_month)
 ]
